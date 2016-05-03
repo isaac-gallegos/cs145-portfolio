53c77eef2b6680bda91e638a6c90ea65249f5888
isaac-gallegos <isaac-gallegos@outlook.com>
2016-05-03 16:59:40 -0600

    'third_change'

--- MyPortfolio.java
+++ MyPortfolio.java
@@ -27,7 +27,10 @@ class MyPortfolio {
      * the screen on one line.
      */	
 	static void printIntegerList(int[] values) {
-        System.out.print(values);
+        int j = values.length;
+        for (int i = 0; i < j; i++) {
+            System.out.print(values[i] + " ");
+        }
 	}
 
 
@@ -36,13 +39,10 @@ class MyPortfolio {
      * ORDER to the screen on one line.
      */	
 	static void printIntegerListReversed(int[] values) {
-        int[] new_list;
-        int j = values.length;
-        for (int i = 0; i < values.length; i++) {
-            new_list[i] = values[j];
-            j = j - 1;
+        int j = values.length - 1;
+        for (int i = j; i >= 0; i--) {
+            System.out.print(values[i] + " ");
         }
-        System.out.print(values);
 	}
 
     /**
@@ -50,7 +50,13 @@ class MyPortfolio {
      * and returns the "new" left_char.
       */	
 	static char swapLeftWithRight(char left_char, char right_char) {
-		return ' ';
+        char x = left_char;
+        char y = right_char;
+        char z = left_char;
+
+        x = y;
+        y = z;
+		return x;
 	}
 
     /**
@@ -59,7 +65,12 @@ class MyPortfolio {
      * containing the swapped version of the array passed in as the argument.
      */	
 	static int[] swapFirstWithLast(int[] values) {
+        int x = values[0];
+        int y = values[values.length - 1];
+        int z = values[0];
 
+        values[0] = y;
+        values[values.length-1] = z;
 		return values;
 	}
 
@@ -68,8 +79,14 @@ class MyPortfolio {
      * found in the list.
      */	
 	static int min(int[] values) {
+        int minimum = values[0];
 
-		return 0;
+        for(int i = 0; i < values.length; i++) {
+            if (values[i] < minimum) {
+                minimum = values[i];
+            }
+        }
+		return minimum;
 	}
 
     /**
@@ -77,17 +94,28 @@ class MyPortfolio {
      * found in the list.
      */	
 	static int max(int[] values) {
+        int maximum = values[0];
 
-		return 0;
-	}
+        for(int i = 0; i < values.length; i++) {
+            if (values[i] > maximum) {
+                maximum = values[i];
+            }
+        }
+        return maximum;
+    }
 
     /**
      * Accepts a list of integers as an argument and returns the average of all the
      * values found in the list.
      */	
 	static int findAverage(int[] values) {
+        int sum = 0;        
 
-		return 0;
+        for(int i = 0; i < values.length; i++) {
+            sum = sum + values[i];
+        }
+        int average = (sum / values.length);
+		return average;
 	}
 
     /**
@@ -95,8 +123,13 @@ class MyPortfolio {
      * the number of times the integer is found in the list.
      */	
 	static int frequencyCount(int[] values, int val) {
-
-		return 0;
+        int count = 0;
+        for(int i = 0; i < values.length; i++) {
+            if (val == values[i]) {
+                count++;
+            }
+        }
+		return count;
 	}
 
     /**
